278d0d9753d33cb8fe2f771bf61f9a95cbd1024e
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-10-16 22:06:52 +0200

    Added `LoginCard` component

--- src/components/LoginCard.js
+++ src/components/LoginCard.js
@@ -1,13 +1,50 @@
-import { Card } from "@mui/material";
+import { ArrowBack } from "@mui/icons-material";
+import { Paper, Typography, Link } from "@mui/material";
+import LoginForm from "./LoginForm";
 
-export default function LoginCard({ logo, children }) {
+export default function LoginCard() 
+{
     return (
-        <div className="">
-            <div>{ logo }</div>
+        <Paper elevation={10} sx={{
+            padding: "50px 40px",
+            maxWidth: "400px",
+            maxHeight: "800px",
+            borderRadius: "7px",
+            flexGrow: "1"
+        }}>
+            <Typography 
+            variant="h1" 
+            color="#303030"
+            fontSize="2.5rem"
+            fontWeight="bold"
+            fontFamily="Segoe UI"
+            textAlign="center"
+            > 
+                Login 
+            </Typography>
+            
+            <LoginForm />
 
-            <Card>
-                { children }
-            </Card>
-        </div>
+            <Link href="#" marginTop={2} sx={{ textDecoration: 'none'}}>                
+                <ArrowBack 
+                fontSize="inherit"
+                sx={{
+                    position: "relative",
+                    bottom: "-3px"
+                }}
+                />
+
+                <Typography 
+                display="inline"
+                variant="body2" 
+                color="initial" 
+                letterSpacing="1px"
+                marginLeft="5px"
+                >
+                    Forgot password?
+                </Typography>
+            </Link>
+
+        </Paper>
     );
 }
