3ef30ed0c07b476dcb773a123c2542329b0894c3
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-10-16 22:06:09 +0200

    Renamed `GuestLayout` to `LoginLayout`

---
+++ src/components/layouts/LoginLayout.js
@@ -0,0 +1,22 @@
+import { Grid } from "@mui/material";
+
+export default function LoginLayout({ children }) {
+    return (
+        <Grid
+        container
+        item
+        height="100vh"
+        alignItems={"center"}
+        justifyContent={"center"}
+        alignContent={"center"}
+        sx={{
+            backgroundImage: "url(/background.png)",
+            backgroundRepeat: "repeat",
+            backgroundColor: "#A57A2A",
+            backgroundPosition: "-10px 50px"
+        }}
+        >
+            { children }
+        </Grid>
+    );
+}
