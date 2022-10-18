d619dc96b2b7d55fcdffa77444287bf6a3c4e185
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-10-18 21:16:25 +0200

    Created `ForgotPassword` page

---
+++ src/pages/forgot-password.js
@@ -0,0 +1,11 @@
+import GuestLayout from '../components/layouts/GuestLayout';
+import ForgotPasswordCard from '../components/ForgotPasswordCard';
+
+export default function ForgotPassword()
+{
+    return (
+        <GuestLayout>
+            <ForgotPasswordCard />
+        </GuestLayout>
+    );
+}
