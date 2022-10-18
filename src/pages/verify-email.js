3d10f46fed0f8b18cdebf085e9072c7e37818f0d
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-10-18 10:52:48 +0200

    Created `verify-email' page

---
+++ src/pages/verify-email.js
@@ -0,0 +1,10 @@
+import GuestLayout from "../components/layouts/GuestLayout";
+
+export default VerifyEmail()
+{
+    return (
+        <GuestLayout>
+            
+        </GuestLayout>
+    );
+}
