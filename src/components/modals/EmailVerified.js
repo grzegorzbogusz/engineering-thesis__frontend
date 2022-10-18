22506a2c1532da1a955edebe9c9c6c869618cb2c
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-10-18 15:52:03 +0200

    Added `EmailVerified` component

---
+++ src/components/modals/EmailVerified.js
@@ -0,0 +1,33 @@
+import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, Typography } from "@mui/material";
+
+export default function EmailVerified({ open, setOpen })
+{
+    return (
+        <Dialog open={open} aria-labelledby={"Email verified"}>
+          <DialogTitle id={"emailVerified"}>
+            <Typography 
+            variant="h6" 
+            color="#303030"
+            fontFamily="Segoe UI"
+            fontWeight={"bold"}
+            >
+                Email verified!
+            </Typography>
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText maxWidth="400px">
+                Email successfully verified
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions sx={{ display: "flex", justifyContent: "center"}}>
+            <Button
+              onClick={() => setOpen(false)}
+              color="primary"
+              variant="outlined"
+            >
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+    );
+}
